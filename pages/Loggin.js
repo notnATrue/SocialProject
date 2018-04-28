8c6bb9142596d55085ce2e1b748528f9c2c08e29
Tony Trubchaninov <notnatrue@gmail.com>
2018-04-28 15:44:26 +0300

    SocProject_v2.0

---
+++ pages/Loggin.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    var slider = document.querySelector(".card.mb-3.fixed-bottom.bg-dark");
+    
+    var loginbtn = document.querySelector('.btn.btn-outline-primary');
+    loginbtn.addEventListener('click' , loggin);
+
+    var signbtn = document.querySelector('#submitlog');
+    signbtn.addEventListener('click', loggin);
+
+    function loggin(event){
+        logg(event.target);
+    };
+
+    function logg(t){
+        console.log(t);
+    };
+};
+
